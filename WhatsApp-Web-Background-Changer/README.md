f9bdc96ad27d26c4c7c87fb5f7c0b591bb3975c0
Kunal Gupta <kunal.gupta@myself.com>
2018-06-03 20:46:27 +0530

    Updated for the newer Whatsapp Web.

--- WhatsApp-Web-Background-Changer/README.md
+++ WhatsApp-Web-Background-Changer/README.md
@@ -4,15 +4,9 @@ This script can change the background image of the WhatsApp Web interface.
 ## Installing
 1. Install the script from [GreasyFork](https://greasyfork.org/en/scripts/29884-whatsapp-web-background-changer) or [OpenUserJS](https://openuserjs.org/scripts/iamKunal/WhatsApp_Web_Background_Changer) after following [the instructions to install TamperMonkey/GreaseMonkey](https://github.com/iamKunal/UserScripts/blob/master/README.md).
 2. Reload/Open the WhatsApp Web interface.
-3. Open up a chat and select `Change Background` from the menu to the right of Attachment Button.
-
-!['Change Background' Screenshot](/../master/WhatsApp-Web-Background-Changer/assets/screenshot_change_wallpaper.png?raw=true "Change Background")
-
+3. Press Ctrl+B on your keyboard to open up image Selection.
 4. Browse and select the image you want to select as Wallpaper.
-5. Press the respective button to set the selected wallpaper for All Chats or the Current Chat.
-
-!['Set Wallpaper Dialog' Screenshot](/../master/WhatsApp-Web-Background-Changer/assets/screenshot_confirm_dialog.png?raw=true "Setting the Wallpaper")
-
+5. Press OK to set the wallpaper for all chats.
 6. Wait a few seconds for the image to load.
 
 ## Updating
--- WhatsApp-Web-Background-Changer/whatsapp-web-background-changer.user.js
+++ WhatsApp-Web-Background-Changer/whatsapp-web-background-changer.user.js
@@ -2,8 +2,8 @@
 // @id           WhatsApp Web Background Changer
 // @name         WhatsApp Web Background Changer
 // @namespace    https://www.github.com/iamKunal
-// @version      3.2.1
-// @description  Changes the WhatsApp Web Chat Wallpaper/Background. One can even select different wallpapers for different chats.
+// @version      4.0.0
+// @description  Change WhatsApp Web Chat Background
 // @author       Kunal Gupta < kunal.gupta@myself.com >
 // @icon         https://github.com/iamKunal/UserScripts/raw/master/WhatsApp-Web-Background-Changer/assets/wa-bg.png
 // @icon64       https://github.com/iamKunal/UserScripts/raw/master/WhatsApp-Web-Background-Changer/assets/wa-bg64.png
@@ -13,41 +13,17 @@
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
 
-//The default Wallpaper to Load
+
+// ==/UserScript==
 var bgURLold="https://github.com/iamKunal/UserScripts/raw/master/WhatsApp-Web-Background-Changer/assets/default_wallpaper.jpg";
-
-//HTML for The Confirmation Dialog
-var dialogHTML=`<div tabindex="-1" class="" style="opacity: 1;"><div tabindex="-1"><div class="backdrop"><div class="popup-container"><div class="popup" style="opacity: 1; transform: scaleX(1) scaleY(1);"><div class="popup-body">
-<div class="popup-title">Change Chat Wallpaper</div>
-<div class="popup-contents"><span class="emojitext" dir="auto">Which chat would you like to apply this wallpaper to?</span></div></div>
-<div class="popup-controls">
-<button class="btn-plain popup-controls-item">Cancel</button>
-<button class="btn-plain btn-default popup-controls-item">All Chats</button>
-<button class="btn-plain btn-default popup-controls-item">This Chat</button>
-</div></div></div></div></div></div>`;
-
-
+var bgImage;
 (function() {
     'use strict';
-    var checkExist = setInterval(function() {
+
+    var bgImage = GM_getValue("bgURL",bgURLold);
+        var checkExist = setInterval(function() {
         if (!$('#startup').length) { //Check for Content to Load
             clearInterval(checkExist);
-            getInputReady();
-            document.onclick= function(event) {
-                getMenuReady();
-                updateWP();
-            };
-        }
-    }, 100);
-
-
-})();
-function updateWP(){
-    var chatName = document.querySelector('.active.chat .chat-body .chat-main .chat-title .emojitext.ellipsify').title;
-    var bgImage = GM_getValue(chatName,GM_getValue("bgURL",bgURLold));
-     document.getElementsByClassName("pane-chat-msgs pane-chat-body lastTabIndex")[0].setAttribute("style","background-position: center;background-image: url(" + bgImage+"); -webkit-transition: background-image 0.2s ease-in-out; transition: background-image 0.2s ease-in-out;");
-}
-function getInputReady(){
     var a=document.getElementsByTagName("body")[0];
     //node.addEventListener('click',askBG,false);
     var input = document.createElement("input");
@@ -56,10 +32,14 @@ function getInputReady(){
     input.style.display="none";
     input.accept="image/*";
     input.onchange=function(){
-        fr = new FileReader();
+        var fr = new FileReader();
         fr.onload = function(){
             if(input.files!==null){
-                getDialogInput(fr.result);
+                var thisChat = null;
+                thisChat = confirm("Press OK to confirm setting the selected wallpaper for all chats.");
+                GM_setValue("bgURL",fr.result);
+                //document.body.click();
+                updateWP();
             }
         };
         if(input.files!==null){
@@ -67,53 +47,38 @@ function getInputReady(){
         }
     };
     a.appendChild(input);
-}
-function askBG(){
+
     document.getElementById("wa-bg-change-input").click();
-}
-function getMenuReady(){
-    var checkExist = setInterval(function() {
-        if (document.querySelector("#main > header > div.pane-chat-controls > div > div:nth-child(3) > button")!==null) { //Check for Content to Load
-            clearInterval(checkExist);
-            if(document.querySelector("#main > header > div.pane-chat-controls > div > div.menu-item.active > span > div")!==null && document.querySelectorAll("#main > header > div.pane-chat-controls > div > div.menu-item.active > span > div > ul > li").length==5){
-                lst=document.querySelector("#main > header > div.pane-chat-controls > div > div.menu-item.active > span > div > ul");
-                ele=document.querySelector("#main > header > div.pane-chat-controls > div > div.menu-item.active > span > div > ul > li").cloneNode();
-                ele_a=document.querySelector("#main > header > div.pane-chat-controls > div > div.menu-item.active > span > div > ul > li > a").cloneNode();
-                var cb="Change Background";
-                ele_a.title=cb;
-                ele_a.textContent=cb;
-                ele.addEventListener('click',askBG,false);
-                ele.addEventListener('mouseover',(function(){ele.style.boxShadow="inset 20px 0px 20px 11px #f4f5f5";}));
-                ele.addEventListener('mouseout',(function(){ele.style.boxShadow="";}));
-                ele.appendChild(ele_a);
-                ele.classList.add("wa-bg-change-button");
-                ele.setAttribute("style",ele.getAttribute("style")+"background: url(https://github.com/iamKunal/UserScripts/raw/master/WhatsApp-Web-Background-Changer/assets/wa-bg.png); background-repeat: no-repeat; background-size: 25px; background-position-x: 90%; background-position-y: 5px;");
-                lst.appendChild(ele);
-            }
+
         }
     }, 100);
-}
-function getDialogInput(result){ // Uses the WhatsApp Web DialogUI
-    var div = document.createElement("span");
-    div.id="Custom-Alerter";
-    div.innerHTML=dialogHTML;
-    acd=document.querySelector("#app > div");
-    acd.appendChild(div);
-    var buttonsPath="#Custom-Alerter > div > div > div > div > div > div.popup-controls";
+    $(document).keydown(function(e){
+        if (e.keyCode==66 && e.ctrlKey)
+            $('#wa-bg-change-input').click();
+    });
+    updateWP();
+    })();
 
-    var buttonsList=document.querySelector(buttonsPath).children;
-    buttonsList[0].onclick=(function(){ //Cancel
-        acd.lastChild.remove();
-    });
-    buttonsList[1].onclick=(function(){ //All Chats
-        GM_setValue("bgURL",fr.result);
-        document.body.click();
-        acd.lastChild.remove();
-    });
-    buttonsList[2].onclick=(function(){ //This Chat
-        var chatName = document.querySelector('.active.chat .chat-body .chat-main .chat-title .emojitext.ellipsify').title;
-        GM_setValue(chatName,fr.result);
-        document.body.click();
-        acd.lastChild.remove();
-    });
+
+function updateWP(){
+    var bgImage = GM_getValue("bgURL",bgURLold);
+    if ($('#wa-bg-css' != null)){
+        $('#wa-bg-css').innerHTML=''
+    }
+    injectHTML(`
+<div id="wa-bg-css"><style>
+[data-asset-chat-background]{
+background-image:url('` + bgImage + `') !important;
+background-position: center;
+opacity: 1.0;
+}
+</style></div>
+`);
+
+}
+
+function injectHTML(rule) {
+    var div = $("<div />", {
+        html: '' + rule + ''
+    }).appendTo("body");
 }
